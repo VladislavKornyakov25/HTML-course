d623e9a6784d4dfa397741bbc3bc2f76bb21e54b
Vladislav Kornyakov <time2begin666@gmail.com>
2024-05-31 13:17:36 +0300

    Desctop version 1.0

--- Main/TaskForVTOP3/js/script.js
+++ Main/TaskForVTOP3/js/script.js
@@ -1,31 +1,11 @@
 const langBtn = document.querySelector('#lang-button');
-// console.log(langBtn);
-// const modifiedItem = document.createElement('span');
-// modifiedItem.textContent = 'RU';
-// langBtn.addEventListener('click', (event) => {
-//     console.log('event listener ' + event.currentTarget);
-//     console.log(langBtn.innerHTML);
-//     //langBtn.replaceChild(modifiedItem, langBtn.span);
-//     //langBtn.textContent = 'RU';
-//     langBtn.innerHTML = `
-//         <a href="#">
-//             <img src="./icons/Planet.png" alt="lang">
-//             <span>RU</span>
-//         </a>
-//     `;
-// });
+const langSpan = document.querySelectorAll('.lang');
 
-$('#lang-button').on('click', function (button) {
-    button.preventDefault();
-    console.log($(this.childNodes));
-    console.log('20');
-    // if ($(this).hasClass('ru')) {
 
-    //     $(this).toggleClass('ru en').text('EN');
-    //     //loadNewLang('en');
-    // } else if ($(this).hasClass('en')) {
-    //     $(this).toggleClass('en ru').text('RU');
-    //     //loadNewLang('ru');
-    // }
+langBtn.addEventListener('click', () => {
+    langSpan.forEach(item => {
+        item.classList.contains('hide')
+            ? item.classList.remove('hide')
+            : item.classList.add('hide');
+    });
 });
-
