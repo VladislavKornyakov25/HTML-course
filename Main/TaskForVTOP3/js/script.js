69750807648b2a5428c07c09bbb17e847c969a07
Vladislav Kornyakov <time2begin666@gmail.com>
2024-06-04 17:54:56 +0300

    Finish?

--- Main/TaskForVTOP3/js/script.js
+++ Main/TaskForVTOP3/js/script.js
@@ -9,3 +9,23 @@ langBtn.addEventListener('click', () => {
             : item.classList.add('hide');
     });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+        menuItem = document.querySelectorAll('.nav-menu__list-item'),
+        hamburger = document.querySelector('.header-menu__icon'),
+        mobileMenu = document.querySelector('.header-mobile');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+        mobileMenu.classList.toggle('invisible');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
